8f491dce3eae25f6fe5b05560f1a6278639c4845
Tert <56269917+Tertenea@users.noreply.github.com>
2022-10-12 23:30:13 +0300

    Create harjutus01.py

---
+++ harjutus01.py
@@ -0,0 +1,9 @@
+#P.Kiviorg
+#12.10.2022
+#Harjutus 1
+print("\"Cabernet Sauvignon\" või \'Cabernet Sauvignon\'?")
+print("  @..@\n (----)\n( \__/ )\n^^ \"\" ^^")
+
+x=int(input("Sisesta ruudu külje pikkus: "))
+y=x*x
+print("Vastus: ruudu pindala on " + str(y) + "cm^2")
