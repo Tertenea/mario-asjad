4359d3966b966f06f34731b69f28997744a13602
Tert <56269917+Tertenea@users.noreply.github.com>
2022-10-17 15:21:10 +0300

    Update mingi poolik asi.py

--- mingi poolik asi.py
+++ mingi poolik asi.py
@@ -14,9 +14,23 @@ else:
     print(tekst)
 
 email=input("\nSisesta oma emaili address: ")
-if "@" in email:
-    print("True")
-else:
-    print("False")
-    
+print("@" in email)
 
+
+a1,a2 = "8:30","14:15"
+h1,m1 = a1.split(":")
+minut1 = int(h1)*60+int(m1)
+h2,m2 = a2.split(":")
+minut2 = int(h2)*60+int(m2)
+
+ajavahe = minut2 - minut1
+hh = ajavahe//60
+mm = ajavahe%60
+print(f"\nAjavahe on {hh}:{mm}")
+
+s6na = input("Sisesta sõna: ")
+def isPalindrome(s6na): 
+    if (s6na == s6na[::-1]):
+        return "See on palindroom."
+    else:
+        return "See ei ole palindroom." 
