93e37de958758dcc95d93e5a2b9d522f6d4f2c5e
Tert <56269917+Tertenea@users.noreply.github.com>
2022-10-19 09:28:46 +0300

    Update harjutus04.py

--- harjutus04.py
+++ harjutus04.py
@@ -3,6 +3,7 @@
 #Harjutus 4
 import random
 
+#ruut
 a,b = 10,20
 if a==b:
     print(f"{a} ja {b} teevad kokku ruudu")
@@ -13,6 +14,7 @@ a = int(input("Number 1: "))
 b = int(input("Number 2: "))
 tehe = input("Vali tehe (+ - * /): ")
 
+#kalkulaator
 if tehe=="+":
     vastus = a + b
 elif tehe=="-":
@@ -25,24 +27,33 @@ else:
     vastus = "n/a"
 print(f"{a}{tehe}{b}={vastus}")
 
+#juubel
 sp = "5.6.2000"
 print("Juubel") if 2022-int(sp.split(".")[2])%5==0 else print("Ei ole juubel")
 
+#soodustus
 hind = float(input("Sisesta toote hind: "))
 if hind>10:
     print("Soodustus 20%")
 else:
     print("Soodustus 10%")
 
+#jalka meeskond
 sugu = input("Sisesta oma sugu (mees või naine): ")
 if sugu=="mees":
     vanus = int(input("Sisesta oma vanus: "))
     if 16<=vanus<=18:
         print("sobid meeskonda")
+    elif vanus<16:
+        print("tõmba nahhui tatt")
+    elif vanus>18:
+        print("goodbye boomer")
     else:
         print("mine ära")
 else:
     print("mine ära")
+
+#tärnid
 print()
 for i in range (5):
     print("* "*5)
@@ -59,34 +70,58 @@ for i in range (5):
     print("* "*y)
     y-=1
 
+#loto
 for i in range (5):
-    print(random.randint(1,9))
+    print(random.randint(0,9),end="")
+print()
 
-z=1
-for i in range (100):
-    if z%2==0:
-        print(f"{z} on paaris arv")
+#paaris paaritu
+for i in range (1,101):
+    if i%2==0:
+        print(f"{i} on paaris")
     else:
-        print(f"{z} on paaritu arv")
-    z+=1
-        
-x=1
-for i in range (10):
-    print(f"5 x {x} + {5*x}")
+        print(f"{i} on paaritu")
+
+#5 korrutus tabel
+arv = 5
+for i in range (1,11):
+    print(f"{arv} x {i} = {arv*i}")
+
+#viisikud
+for i in range (1,101):
+    if i%5==0:
+        print(i)
+
+#mäng
+loop = 1
+
+while loop==1:
+    x=random.randint(1,5)
+    for i in range (3):
+        arva=int(input("Arva arv 1-5: "))
+        if arva==x:
+            print("tubli")
+            break
+        else:
+            print("proovi uuesti loll luud")
+    loop=0
+
+#pank
+kontoraha = 10000
+intress = 0.05 #5%
+aastad = 6
+x=0
+for i in range (aastad):
+    print(f"sul on raha olnud pangas {x} aastat")
+    print (f"arvel on {kontoraha}")
+    kontoraha+=kontoraha*intress
     x+=1
-    
-x=1
-for i in range (100):
-    if x%5==0:
-        print(x)
-    x+=1
 
-x=random.randint(0,9)
-for i in range (3):
-    arva=int(input("Arva number 0-9: "))
-    if arva==x:
-        print("Tubli")
-    else:
-        print("proovi uuesti loll luud")
-
-
+#ruutude ja kuupide tabel
+number=1
+for i in range (1,11):
+    print(number,end="  ")
+    print(number**2,end="  ")
+    print(number**3,end="  ")
+    print()
+    number+=1
