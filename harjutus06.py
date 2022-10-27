d7807883c24758a9e02b2668a038456677c81f7f
Tert <56269917+Tertenea@users.noreply.github.com>
2022-10-27 14:48:42 +0300

    Update harjutus06.py

--- harjutus06.py
+++ harjutus06.py
@@ -1,24 +1,33 @@
 #P.Kiviorg
 #25.10.2022
 #Harjutus 6
-with open("s6pru_l6ustaraamatus.txt", "r") as nimekiri:
+fail=open("s6pru_l6ustaraamatus.txt","r")
 
-    sisu = nimekiri.readlines()
-    sots = 0
-    kesk = 0
-    isamaa = 0
-    reform = 0
+sots = 0
+kesk = 0
+isamaa = 0
+reform = 0
+erakonnad = []
 
-    for rida in sisu:
-        if rida.find("SDE") == 0:
-            sots+=1
-        elif rida.find("KE") == 0:
-            kesk+=1
-        elif rida.find("IRL") == 0:
-            isamaa+=1
-        elif rida.find("RE") == 0:
-            reform+=1
-        else:
-            print("katki")
+for i in fail.readlines():
+    enimi,pnimi,erak,kyl = i.split()
+    if erak == "SDE":
+        sots+=1
+    elif erak == "KE":
+        kesk+=1
+    elif erak == "IRL":
+        isamaa+=1
+    elif erak == "RE":
+        reform+=1
+    if erak not in erakonnad:
+        erakonnad.append(erak)
+    with open("nimed.txt","a") as nimedefail:
+        nimedefail.write(f"{enimi} {pnimi}\n")
 
+print(f"Erakondi kokku on {len(erakonnad)}")
+print(f"Sotse on {sots}")
+print(f"Kesikuid on {kesk}")
+print(f"IRLikaid on {isamaa}")
+print(f"Reformikaid on {reform}")
 
+fail.close()
