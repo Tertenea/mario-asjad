c23a9ce3ae5310a9c0505f6fa964b5af9e7b7155
Tert <56269917+Tertenea@users.noreply.github.com>
2023-01-10 09:08:58 +0200

    Create gpython03.py

---
+++ python/graafiline/gpython03.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+aken = Tk()
+aken.title('Tervitus')
+aken.iconbitmap('favicon.ico')
+aken.configure(background='orange')
+aken.resizable(0, 0)
+Label(aken, text='Nimi: Peeter Kiviorg', font='Tahoma 16 bold italic', fg='pink', bg='orange', pady=10, padx=30).pack()
+Label(aken, text='Rühm: IT22', font='Tahoma 16 bold italic', fg='pink', bg='orange', pady=10, padx=30).pack()
+Label(aken, text='2023', font='Tahoma 16 bold italic', fg='pink', bg='orange', pady=10, padx=30).pack()
+
+aken.mainloop()
