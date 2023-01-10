374ed99a0a871a0063b0153dda5b76e7e4620bf9
Tert <56269917+Tertenea@users.noreply.github.com>
2023-01-10 09:19:03 +0200

    Update gpython03.py

--- python/graafiline/gpython03.py
+++ python/graafiline/gpython03.py
@@ -1,12 +1,16 @@
 from tkinter import *
 
+nimi = input('Sisesta oma täis nimi: ')
+ryhm = input('Sisesta oma rühm: ')
+aasta = int(input('Sisesta praegune aasta: '))
+
 aken = Tk()
 aken.title('Tervitus')
 aken.iconbitmap('favicon.ico')
 aken.configure(background = 'orange')
 aken.resizable(0, 0)
-Label(aken, text = 'Nimi: Peeter Kiviorg', font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
-Label(aken, text = 'Rühm: IT22', font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
-Label(aken, text = '2023', font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
+Label(aken, text = f'Nimi: {nimi}', font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
+Label(aken, text = f'Rühm: {ryhm}', font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
+Label(aken, text = aasta, font = 'Tahoma 16 bold italic', fg = 'pink', bg = 'orange', pady = 10, padx = 30).pack()
 
 aken.mainloop()
